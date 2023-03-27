7ed10645d89d268e77f4bd700b684f4f1996f36e
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-03-27 22:52:21 +0300

    2023.03.20 Pazartesi - 1.Ders - 2.Kısım

--- ProductProject/Controllers/DefaultController.cs
+++ ProductProject/Controllers/DefaultController.cs
@@ -13,5 +13,22 @@ namespace ProductProject.Controllers
         {
             return View();
         }
+
+        //public string deneme()
+        //{
+        //    return "şsdfşla";
+        //}
+
+        public ActionResult Index2()
+        {
+            return View(); // View dediğimiz şey bir sayfa. Yani geriye bize bir tane sayfa döndürecek
+        }
+
+        // MVC de default olarak View oluşturmak için verdiği metot türü bu
+        // Sağ tıkla View oluşturmamızı sağlayan metot türü ActionResult
+        public ActionResult Index3()
+        {
+            return View();
+        }
     }
 }
