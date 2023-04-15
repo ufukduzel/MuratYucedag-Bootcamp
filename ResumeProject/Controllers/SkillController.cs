852c92d5d5018622f44158505d6709220d7b0974
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-04-15 23:31:22 +0300

    2023.04.03 Pazartesi - 5.Ders - 1.Kısım

--- ResumeProject/Controllers/SkillController.cs
+++ ResumeProject/Controllers/SkillController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ResumeProject.Models;
 
 namespace ResumeProject.Controllers
 {
     public class SkillController : Controller
     {
         // GET: Skill
+
+        DbResumeEntities db = new DbResumeEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var values = db.TblSkill.ToList();
+            return View(values);
         }
+
+        // Index sayfasında yer alan (küçük) model ve (büyük) Model arasındaki fark araştırılacak.
     }
 }
