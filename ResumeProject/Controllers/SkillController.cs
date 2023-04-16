c801e3f60b54acad02d161437d60ef1800c5ae08
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-04-17 00:48:32 +0300

    2023.04.03 Pazartesi - 5.Ders - 2.Kısım

--- ResumeProject/Controllers/SkillController.cs
+++ ResumeProject/Controllers/SkillController.cs
@@ -11,7 +11,16 @@ namespace ResumeProject.Controllers
     {
         // GET: Skill
 
+        // return View() aynı sayfayı tekrar döndürür
+        // return RedirectToAction() başka sayfaya yönlendirmeyi sağlar
+
         DbResumeEntities db = new DbResumeEntities();
+        // Entity Framework metodları:
+        // ToList() listeleme yapar
+        // Add() ekleme işlemi yapar
+        // SaveChanges() değişiklikleri veritabanına yansıtır
+        // Find() genel olarak id ye göre arama işlemi yapar
+        // Remove() silme işlemi uygular
 
         public ActionResult Index()
         {
@@ -20,5 +29,48 @@ namespace ResumeProject.Controllers
         }
 
         // Index sayfasında yer alan (küçük) model ve (büyük) Model arasındaki fark araştırılacak.
+
+        [HttpGet] // Sayfa sadece yüklenirken olan işlemler, tasarımı getirme
+        public ActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost] // Veri girdikten sonra butona tıklama işlemi
+        public ActionResult AddSkill(TblSkill p)
+        {
+            // db entities veritabanını tutuyor
+            db.TblSkill.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // Eğer HttpGet ve HttpPost diye ayırmazsak
+        // sayfadaki verileri boş olarak kaydeder
+
+        // Build, Rebuild, Clean, Batch Build ne anlama geliyor anlatılacak
+        // Build Solution komple bütün projenin derlenmesini sağlar
+
+        // Silme işlemi id ye göre olur.
+        // Silme ve güncelleme işlemlerinde verinin getirilmesi işlemlerinde 
+        // parametrenin mutlaka id olması gerekiyor. Özel bir sebebi var.
+        // Özel sebebi RouteConfig.cs deki ayarla ilgilidir.
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = db.TblSkill.Find(id);
+            db.TblSkill.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Güncelleme işlemi iki aşamada gerçekleştirilir.
+        // Güncellenecek verilerin sayfaya taşınması ve güncellenme işlemi
+        [HttpGet] // Güncellenecek verinin getirilme aşaması
+        public ActionResult UpdateSkill(int id) // Html.Helpers yapısıyla
+        {
+            var value = db.TblSkill.Find(id);
+            return View(value); 
+            // id 5 ise value içinde id si 5 olan verinin bütün sütunları olur
+        }
+
+        // HttpPut genel olarak api lerde kullanılır
     }
 }
