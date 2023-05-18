af80431f2d7e19e2548f69f9f34681549e25dd91
Ufuk.Duzel <ufukduzel57@gmail.com>
2023-05-18 18:51:24 +0300

    2023.04.12 Çarşamba - 4.Hafta - 8.Ders - 1.Kısım

--- ResumeProject/Controllers/DefaultController.cs
+++ ResumeProject/Controllers/DefaultController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ResumeProject.Models;
 
 namespace ResumeProject.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+
+        DbResumeEntities db = new DbResumeEntities();
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblProfile.ToList();
+            return PartialView(values);
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
+
+        
     }
 }
